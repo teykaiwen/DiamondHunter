5ec25915ef13440c07a301ac987bafbea3b06246
teykaiwen <tey.kaiwen@gmail.com>
2018-11-29 20:54:09 +0800

    textfield for input coordinates added

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,22 +1,48 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import com.neet.DiamondHunter.Main.*;
 
 public class Controller {
 
     @FXML
-    private AnchorPane ok_button;
+    private ImageView boat;
+
     @FXML
-    private ImageView boat = new ImageView("D:\\Documents\\Computer Science\\Year 2\\Software Maintenance\\Coursework2\\SRC\\DiamondHunter\\Resources\\boat");
-       
-    public void printOKbutton() {
-		System.out.println("Program is running..");
-		com.neet.DiamondHunter.Main.Game.main(null);
-	}
-    
-    
+    private ImageView axe;
+
+    @FXML
+    private TextField x_Axe;
+
+    @FXML
+    private TextField y_Axe;
+
+    @FXML
+    private TextField x_Boat;
+
+    @FXML
+    private TextField y_Boat;
+
+    @FXML
+    void get_X_Axe(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void get_X_Boat(ActionEvent event) {
+
+    }
+
+    @FXML
+    void get_Y_Axe(ActionEvent event) {
+
+    }
+
+    @FXML
+    void get_Y_Boat(ActionEvent event) {
+
+    }
 
 }
