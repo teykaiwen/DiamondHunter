476cbcf9bbb5c5be2d1370527f82e5d3bea454cf
hanward28 <hanward28@gmail.com>
2018-12-03 14:44:51 +0800

    Function for setting boat and axe coordinates in mapviewer added.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,18 +1,37 @@
 package application;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import com.neet.DiamondHunter.Main.*;
 
 public class Controller {
 
+	String x_axe;
+	String y_axe;
+	String x_boat;
+	String y_boat;
+	
+	@FXML
+    private GridPane gridPane;
+	
     @FXML
-    private ImageView boat;
+    private BufferedImage b;
 
     @FXML
-    private ImageView axe;
+    private BufferedImage a;
 
     @FXML
     private TextField x_Axe;
@@ -28,26 +47,39 @@ public class Controller {
 
     @FXML
     void get_X_Axe(ActionEvent event) {
-    	String s = x_Axe.getText();
+    	x_axe = x_Axe.getText();
+    	//System.out.println(x_axe);
     }
 
     @FXML
     void get_Y_Axe(ActionEvent event) {
-    	String s = y_Axe.getText();
+    	y_axe = y_Axe.getText();
     }
     
     @FXML
     void get_X_Boat(ActionEvent event) {
-    	String s = x_Boat.getText();
+    	x_boat = x_Boat.getText();
     }
 
     @FXML
     void get_Y_Boat(ActionEvent event) {
-    	String s = y_Boat.getText();
+    	y_boat = y_Boat.getText();
     }
 
     @FXML
-	void confirmButton () {
-		com.neet.DiamondHunter.Main.Game.main(null);
-	}
+	void confirmButton (ActionEvent event) throws IOException {
+		//com.neet.DiamondHunter.Main.Game.main(null);
+    	File f_boat = new File("@../../Resources/boat.png");
+    	File f_axe = new File("@../../Resources/axe.png");
+    	b = ImageIO.read(f_boat);
+    	a = ImageIO.read(f_axe);
+    	Image image_boat = SwingFXUtils.toFXImage(b, null);
+    	Image image_axe = SwingFXUtils.toFXImage(a, null);
+       	Integer browIndex = Integer.valueOf(x_boat);
+    	Integer bcolIndex = Integer.valueOf(y_boat);
+    	Integer arowIndex = Integer.valueOf(x_axe);
+    	Integer acolIndex = Integer.valueOf(y_axe);
+    	gridPane.add(new ImageView(image_boat), browIndex, bcolIndex);
+    	gridPane.add(new ImageView(image_axe), arowIndex, acolIndex);
+   	}
 }
