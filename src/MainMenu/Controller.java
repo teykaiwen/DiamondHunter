a4aaf4e460f85f65a63e7fd634dd02e7fdae6b1b
teykaiwen <tey.kaiwen@gmail.com>
2018-12-08 14:46:24 +0800

    run game button functionality added

---
+++ src/MainMenu/Controller.java
@@ -0,0 +1,27 @@
+package MainMenu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import com.neet.DiamondHunter.Main.*;
+import application.Main;
+
+public class Controller {
+
+    @FXML
+    private Button runGameButton;
+
+    @FXML
+    private Button mapViewerButton;
+
+    @FXML
+    void runGameOnClick(ActionEvent event) {
+    	Game.main(null);
+    }
+
+    @FXML
+    void runMapOnClick(ActionEvent event) {
+    	//Main.main(null);
+    }
+
+}
