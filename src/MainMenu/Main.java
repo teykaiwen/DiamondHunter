5dc1c66b981cad6be730d97fc9a90dfb58bb4e22
hanward28 <hanward28@gmail.com>
2018-12-08 14:11:56 +0800

    Add Main.java and refit main menu

---
+++ src/MainMenu/Main.java
@@ -0,0 +1,38 @@
+package MainMenu;
+
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+
+import application.Controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.Parent;
+
+public class Main extends Application{
+	@Override
+	public void start(Stage primaryStage) {
+
+		try {
+
+			Parent root = FXMLLoader.load(getClass().getResource("FrontPage.fxml"));
+			Scene scene = new Scene(root,440,443);
+			scene.getStylesheets().add(getClass().getResource("FrontPageButton.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Diamond Hunter");
+			primaryStage.setResizable(false);
+						
+			primaryStage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
