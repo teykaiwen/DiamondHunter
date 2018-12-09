a8135225413247b6551c0daaa93275e3ee6c615a
hanward28 <hanward28@gmail.com>
2018-12-09 14:50:00 +0800

    MapView button functionality in main menu added - WIP

--- src/MainMenu/Controller.java
+++ src/MainMenu/Controller.java
@@ -2,26 +2,52 @@ package MainMenu;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
 import com.neet.DiamondHunter.Main.*;
 import application.Main;
 
 public class Controller {
-
+		
     @FXML
     private Button runGameButton;
 
     @FXML
     private Button mapViewerButton;
 
+    private Stage stage;
+    
+    
     @FXML
     void runGameOnClick(ActionEvent event) {
     	Game.main(null);
     }
-
+        
     @FXML
-    void runMapOnClick(ActionEvent event) {
-    	Main.main(null);
+    public void runMapOnClick(ActionEvent event) throws IOException {
+    	try 
+    	{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application/MapView.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));  
+            stage.show();
+        } 
+    	catch(Exception e) 
+    	{
+    		e.printStackTrace();
+        }
+    	  	
+    		
     }
 
 }
