783e9913721436510e2d2482fffe09dd2ad198b6
hami0314 <hcyyl1@nottingham.edu.my>
2018-12-13 20:17:27 +0800

    Transition added for Main Menu.

--- src/MainMenu/Controller.java
+++ src/MainMenu/Controller.java
@@ -1,20 +1,33 @@
 package MainMenu;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import com.neet.DiamondHunter.Main.*;
 
-public class Controller {
+public class Controller implements Initializable {
 		
     @FXML
     private Button runGameButton;
 
     @FXML
-    private Button mapViewerButton;    
+    private Button mapViewerButton;   
+    
+    @FXML
+    private ImageView welcomeText;
     
     //Launch Diamond Hunter game window when the run game button is clicked.
     @FXML
@@ -47,5 +60,34 @@ public class Controller {
     	  	
     		
     }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		// Transition of Main Menu Title 
+		FadeTransition fadeTransition = new FadeTransition();
+		fadeTransition.setDuration(Duration.seconds(1.5));
+		fadeTransition.setNode(welcomeText);
+		fadeTransition.setFromValue(0);
+		fadeTransition.setToValue(1);
+		fadeTransition.play();
+		
+		// Transition of runGameButton
+		TranslateTransition playButton = new TranslateTransition();
+		playButton.setDuration(Duration.millis(500));
+		playButton.setNode(runGameButton);
+		playButton.setFromX(-300);
+		playButton.setToX(0);
+		
+		// Transition of mapViewerButton
+		TranslateTransition mapButton = new TranslateTransition();
+		mapButton.setDuration(Duration.millis(500));
+		mapButton.setNode(mapViewerButton);
+		mapButton.setFromX(300);
+		mapButton.setToX(0);
+		
+		SequentialTransition sq = new SequentialTransition(playButton,mapButton);
+		sq.play();
+	}
 
 }
